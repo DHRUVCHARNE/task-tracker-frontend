608b6bd157f71fa9238943439f1b3c9ed5bda9d3
dhruv4ne <dhruv4ne@gmail.com>
2024-12-23 07:37:32 +0000

    Third Commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000', // Backend base URL
+  baseURL: 'https://task-tracker-backend-production-1e86.up.railway.app', // Backend base URL
 });
 
 api.interceptors.request.use((config) => {
